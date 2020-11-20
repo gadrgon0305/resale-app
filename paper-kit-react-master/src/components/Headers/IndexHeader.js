441f2fe1e936721fdf08c337f982c597ff91992e
Gabriel Dragos <gadrgon0305@gmail.com>
2020-11-20 00:46:54 -0200

    changed react theme by reactstrap

---
+++ paper-kit-react-master/src/components/Headers/IndexHeader.js
@@ -0,0 +1,61 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*eslint-disable*/
+import React from "react";
+
+// reactstrap components
+import { Container } from "reactstrap";
+
+// core components
+
+function IndexHeader() {
+  return (
+    <>
+      <div
+        className="page-header section-dark"
+        style={{
+          backgroundImage:
+            "url(" + require("assets/img/antoine-barres.jpg") + ")",
+        }}
+      >
+        <Container className="text-center text-white">
+          <h2>Paper Kit React</h2>
+          <h4>
+            Make your mark with a Free Bootstrap 4 (Reactstrap) UI Kit!
+          </h4>
+        </Container>
+        <h6 className="category category-absolute">
+          Designed and coded by{" "}
+          <a
+            href="https://www.creative-tim.com?ref=pkr-index-page"
+            target="_blank"
+          >
+            <img
+              alt="..."
+              className="creative-tim-logo"
+              src={require("assets/img/creative-tim-white-slim2.png")}
+            />
+          </a>
+        </h6>
+      </div>
+    </>
+  );
+}
+
+export default IndexHeader;
